90a3101425606577d440d7dc065d22df24a7b33e
jojohello <jojohello0758@163.com>
2024-06-18 20:56:50 +0800

    上传Avatar模块内容，未验证

--- Public/webgame/js/extension.js
+++ Public/webgame/js/extension.js
@@ -239,3 +239,235 @@ fgui.GComponent.prototype["dispose"] = function() {
     this._boundsChanged = false;
     Object.getPrototypeOf(fgui.GComponent.prototype).dispose.call(this);
 }
+
+// 提供换装系统，通过修改Laya的Animation类，实现换装
+class AvatarAnimation extends Laya.Animation {
+    constructor() {
+        super();
+        this._init();
+    }
+
+    // 键值对，内容为{partName:[关联的资源名字]}，比如 {“body”:["body", "lefthand", "righthand"]},
+    // 意思就是，比如我们body使用了id为1的衣服，那么身体的资源名字为body_1,左手的资源名字为lefthand_1, 右手的资源名字为righthand_1
+    // 我在这里还是给出基本范例，项目组可以根据自己的情况，自行设值
+    static avatarPartDef = {  "hair":["faxing"], 
+                        "body":["yifu", "uparm_right", "uparm_left", "downarm_left", "downarm_right"], 
+                        "face":["biaoqing"], 
+                        "pants":["kuzi_run_left", "kuzi_run_right", "kuzi_stand_left", "kuzi_stand_right", "kuzi_jump_left", "kuzi_jump_right"],
+                    }; 
+    static rootPath = "character/"; // 资源根目录 
+    // 键值对，内容为{partName:id}，id可以为数字，也可以为字符串，看自己项目组的规定，比如为了美术跟策划便是方便，衣服可以较 buyi（布衣） 盔甲（盔甲）
+    // 那么你可以定义内容为{“body”, buyi},而不是{“body”, 1}
+    // 我在这里还是给出基本范例，项目组可以根据自己的情况，自行设值
+    _avatarPartInfo = {"body":1, "hair":"toukui1", "face":1, "pants":1}; 
+    
+    _texInfo = {}; // 键值对，内容为{部位名字:Texture}，用于存储已经加载的资源
+
+    _id = 0;
+    
+    changePart(partName, id){
+        if(this._avatarPartInfo[partName] == id || !AvatarAnimation.avatarPartDef.hasOwnProperty(partName)){
+            return;
+        }
+
+        this._avatarPartInfo[partName] = id;
+
+        if(this._frames == null)
+            return;
+
+        var subpartNames = AvatarAnimation.avatarPartDef[partName];
+        for(var i = 0; i < subpartNames.length; i++){
+            var texName = subpartNames[i] + "_" + id;
+            this._texInfo[subpartNames[i]] = Laya.loader.getRes(AvatarAnimation.rootPath + texName + ".png");
+
+            for(var j = 0; j < this._frames.length; j++){
+                var frame = this._frames[j];
+                if(frame["texIndexs"] == null)
+                    continue;
+
+                var cmd = frame.cmds[ frame["texIndexs"][subpartNames[i]] ];
+                if(cmd == null)
+                    continue;
+
+                cmd.texture = this._texInfo[subpartNames[i]];
+                cmd.width = this._texInfo[subpartNames[i]].width;
+                cmd.height = this._texInfo[subpartNames[i]].height;
+            }
+        }
+    }
+
+    _init(){
+        // 根据部位定义以及部位信息，缓存贴图资源
+        for(var k in AvatarAnimation.avatarPartDef){
+            var partNames = AvatarAnimation.avatarPartDef[k];
+            for(var i = 0; i < partNames.length; i++){
+                var partName = partNames[i];
+                var texName = partName + "_" + this._avatarPartInfo[k];
+                this._texInfo[partName] = Laya.loader.getRes(AvatarAnimation.rootPath + texName + ".png");
+            }
+        }
+    }
+
+    _setFrames(frames){
+        this._frames = frames;
+        this._count = this._frames.length;
+
+        this._initTexs();
+    }
+
+    _addFrame(newFrame){
+        this._frames.push(newFrame);
+        this._count = this._frames.length;
+    }
+
+    // 遍历所有的帧，将帧中的贴图资源替换成换装后的贴图资源
+    _initTexs(){
+        for(var i = 0; i < this._frames.length; i++){
+            var frame = this._frames[i];
+
+            // 在Graphic新增一个属性，用来记录那个部位在哪个cmd里面
+            if(frame["texIndexs"]){
+                for(var k in frame["texIndexs"]){
+                    var cmd = frame.cmds[ frame["texIndexs"][k]];
+                    cmd.texture = this._texInfo[k];
+                }
+            }else{
+                frame["texIndexs"] = {};
+                for(var j = 0; j < frame.cmds.length; j++){
+                    var cmd = frame.cmds[j];
+                    if(cmd.texture == null)
+                        continue;
+
+                    var texName = cmd.texture.url;
+                    for(var k in this._texInfo){
+                        if(texName.includes(k) == false)
+                            continue;
+
+                        frame["texIndexs"][k] = j;
+                        cmd.texture = this._texInfo[k];
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    _cacheFrames(){
+        if(this._frames == null)
+            return;
+
+        var name = this._actionName;
+        if (this._url)
+            name = this._url + "#" + name;
+
+        if (!AvatarAnimation.framesMap[name]) {
+            AvatarAnimation.framesMap[name] = [];
+        }
+
+        AvatarAnimation.framesMap[name].push(this._frames);
+    }
+
+    // 重写_setFramesFromCache函数
+    _setFramesFromCache(name, showWarn = false) {
+        // ------------ 原代码 ------------
+        // if (this._url)
+        //     name = this._url + "#" + name;
+        // if (name && Animation.framesMap[name]) {
+        //     var tAniO = Animation.framesMap[name];
+        //     if (tAniO instanceof Array) {
+        //         this._frames = Animation.framesMap[name];
+        //         this._count = this._frames.length;
+        //     }
+        //     else {
+        //         if (tAniO.nodeRoot) {
+        //             Animation.framesMap[name] = GraphicAnimation.parseAnimationByData(tAniO);
+        //             tAniO = Animation.framesMap[name];
+        //         }
+        //         this._frames = tAniO.frames;
+        //         this._count = this._frames.length;
+        //         if (!this._frameRateChanged)
+        //             this._interval = tAniO.interval;
+        //         this._labels = this._copyLabels(tAniO.labels);
+        //     }
+            
+        //     return true;
+        // }
+        // else {
+        //     if (showWarn)
+        //         console.log("ani not found:", name);
+        // }
+        // return false;
+        // ------------ 新代码 ------------
+        if (this._url)
+            name = this._url + "#" + name;
+        // 如果队列缓存里面还有，就直接用队列缓存里面的
+        if (name && AvatarAnimation.framesMap[name] && AvatarAnimation.framesMap[name].length > 0) {
+            this._cacheFrames();
+            this._actionName = name;
+            this._setFrames(AvatarAnimation.framesMap[name].pop());
+            return true;
+        }else if (name && Laya.Animation.framesMap[name]) { // 如果队列缓存里面没有，用Animation里面的信息再生成一份
+            var tAniO = Laya.Animation.framesMap[name];
+            if (tAniO instanceof Array) {
+                // 重新生成一份独立的实例
+				//  this._frames = Animation.framesMap[name];
+				var srcFrames = Laya.Animation.framesMap[name];
+                this._cacheFrames();
+                this._actionName = name;
+				this._frames = [];
+				var tempFrame;
+				for(var i = 0; i < srcFrames.length; i++){
+					var newFrame = new Laya.Graphics();
+                    
+					tempFrame = srcFrames[i];
+					for(var j = 0; j < tempFrame.cmds.length; j++) {
+						// this._frames[i].drawImage(tempFrame.cmds[j].texture, tempFrame.cmds[j].x, tempFrame.cmds[j].y, tempFrame.cmds[j].width, tempFrame.cmds[j].height, tempFrame.cmds[j].matrix, tempFrame.cmds[j].alpha);
+                        newFrame._cmds.push(Laya.DrawTextureCmd.create.call(newFrame, tempFrame.cmds[j].texture, tempFrame.cmds[j].x, tempFrame.cmds[j].y, tempFrame.cmds[j].width, tempFrame.cmds[j].height, tempFrame.cmds[j].matrix, tempFrame.cmds[j].alpha, tempFrame.cmds[j].color, tempFrame.cmds[j].blendMode, tempFrame.cmds[j].uv));
+					}
+
+                    this._addFrame(newFrame);
+				}
+				//---------------------------------------------------------------------------------------------------------                
+            }
+            else {
+                if (tAniO.nodeRoot) {
+                    Laya.Animation.framesMap[name] = Laya.GraphicAnimation.parseAnimationByData(tAniO);
+                    tAniO = Laya.Animation.framesMap[name];
+                }
+                // 解析后，再生成一份实例
+                // this._frames = tAniO.frames;
+				var srcFrames = tAniO.frames;
+                this._cacheFrames();
+                this._actionName = name;
+				this._frames = [];
+				var tempFrame;
+				for(var i = 0; i < srcFrames.length; i++){
+					var newFrame = new Laya.Graphics();
+                    newFrame.cmds = [];
+					tempFrame = srcFrames[i];
+
+					for(var j = 0; j < tempFrame.cmds.length; j++) {
+                        newFrame.cmds.push(Laya.DrawTextureCmd.create.call(newFrame, tempFrame.cmds[j].texture, tempFrame.cmds[j].x, tempFrame.cmds[j].y, tempFrame.cmds[j].width, tempFrame.cmds[j].height, tempFrame.cmds[j].matrix, tempFrame.cmds[j].alpha, tempFrame.cmds[j].color, tempFrame.cmds[j].blendMode, tempFrame.cmds[j].uv));
+					}
+
+                    this._addFrame(newFrame);
+				}
+				//---------------------------------------------------------------------------------------------------------
+                this._initTexs();
+                if (!this._frameRateChanged)
+                    this._interval = tAniO.interval;
+                this._labels = this._copyLabels(tAniO.labels);
+            }
+            
+            return true;
+        }
+        else {
+            if (showWarn)
+                console.log("ani not found:", name);
+        }
+        return false;
+    }
+}
+
+window.Laya["AvatarAnimation"] = AvatarAnimation;
+AvatarAnimation.framesMap = {};
